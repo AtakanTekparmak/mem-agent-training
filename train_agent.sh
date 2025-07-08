4ef922a82cdb8f99e19d22031031c776d9bdbfc9
AtakanTekparmak <atakantekerparmak@gmail.com>
2025-07-08 16:26:31 +0200

    fix: Fixed args issue

--- train_agent.sh
+++ train_agent.sh
@@ -42,6 +42,6 @@ uv run python -m openrlhf.cli.train_ppo_ray \
    --vllm_sync_backend nccl \
    --enforce_eager \
    --vllm_enable_sleep \
-   --deepspeed_enable_sleep \ 
-   --use_wandb True \ 
+   --deepspeed_enable_sleep \
+   --use_wandb \
    --wandb_project obsidian-retrieval-openrlhf
