324ef59eb5b2acc768169c04a16187a8506e7f2c
AtakanTekparmak <atakantekerparmak@gmail.com>
2025-07-09 18:16:42 +0200

    fix: Test memory reset

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from training.reward import get_reward
+from training.agent_func import load_static_memory, MEMORY_PATH
 
 def main():
     question = "Who is Lisa Schmidt to my niece, Sophie de Vries?"
@@ -6,6 +7,9 @@ def main():
     ground_truth = "friend(Sophie de Vries)"
     reward = get_reward(question, agent_reply, ground_truth, debug=True)
     print(reward)
+
+    static_memory = load_static_memory()
+    static_memory.reset(MEMORY_PATH)
 
 if __name__ == "__main__":
     main()
