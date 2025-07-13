acc31624716b719b9f4b23b180dc45f9e1576ce5
AtakanTekparmak <atakantekerparmak@gmail.com>
2025-07-13 23:19:59 +0300

    feat: Revert back to original lrs

--- train_agent.sh
+++ train_agent.sh
@@ -18,8 +18,8 @@ export NINJA="$(dirname "$0")/.venv/bin/ninja"
    --kl_estimator k3 \
    --pretrain Qwen/Qwen3-8B \
    --agent_func_path training/agent_func.py \
-   --save_path training/ckpt/qwen3-8b-obsidian-3e-7-3e-6 \
-   --ckpt_path training/ckpt/qwen3-8b-obsidian-3e-7-3e-6 \
+   --save_path training/ckpt/qwen3-8b-obsidian-5e-7-5e-6 \
+   --ckpt_path training/ckpt/qwen3-8b-obsidian-5e-7-5e-6 \
    --advantage_estimator reinforce \
    --save_hf_ckpt \
    --micro_train_batch_size 2 \
@@ -33,8 +33,8 @@ export NINJA="$(dirname "$0")/.venv/bin/ninja"
    --generate_max_len 4096 \
    --zero_stage 3 \
    --bf16 \
-   --actor_learning_rate 3e-7 \
-   --critic_learning_rate 3e-6 \
+   --actor_learning_rate 5e-7 \
+   --critic_learning_rate 5e-6 \
    --prompt_data json@data/openrlhf \
    --input_key context_messages \
    --apply_chat_template \
